d6e1888413be346c7aa03e0e55786bee85cc6909
Wojciech <heiw17@wp.pl>
2022-08-15 17:26:00 +0200

    commit

---
+++ Tc1.1.cy.js
@@ -0,0 +1,31 @@
+///<reference types = 'cypress'/>
+require('cypress-xpath');
+
+describe('Tc#1.1 validation of the "Sizes" component behavior',()=>{
+    before(()=>{
+        cy.visit('/')
+        cy.url().as('currentUrl')
+    })
+    it('"Sizes:"coponent" validation',()=>{
+        cy.get('@currentUrl').then(currentUrl =>{
+            cy.url().then(url =>{
+                expect(url).to.eql(currentUrl)
+            })
+        })
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(2) > label > span').trigger('mouseover').should('be.visible')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(2) > label > span').click().should('have.css','background-color','rgb(27, 26, 32)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(2) > label > span').click().should('have.css','background-color','rgb(236, 236, 236)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(3) > label > span').trigger('mouseover').should('be.visible')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(3) > label > span').click().should('have.css','background-color','rgb(27, 26, 32)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(3) > label > span').click().should('have.css','background-color','rgb(236, 236, 236)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(4) > label > span').trigger('mouseover').should('be.visible')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(4) > label > span').click().should('have.css','background-color','rgb(27, 26, 32)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(4) > label > span').click().should('have.css','background-color','rgb(236, 236, 236)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(5) > label > span').trigger('mouseover').should('be.visible')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(5) > label > span').click().should('have.css','background-color','rgb(27, 26, 32)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(5) > label > span').click().should('have.css','background-color','rgb(236, 236, 236)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(6) > label > span').trigger('mouseover').should('be.visible')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(6) > label > span').click().should('have.css','background-color','rgb(27, 26, 32)')
+        cy.get('#root > div > main > div > div.sc-bj2vay-0.DCKcC > div:nth-child(6) > label > span').click().should('have.css','background-color','rgb(236, 236, 236)')
+    })
+})
